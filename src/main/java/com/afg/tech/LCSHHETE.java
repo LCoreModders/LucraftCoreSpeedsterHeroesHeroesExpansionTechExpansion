ceb60d9289fd339af64835f88bbc0023164f8fb5
AFlyingGrayson <theflyinggrayson01@gmail.com>
2017-09-02 17:48:00 -0500

    initial commit

---
+++ src/main/java/com/afg/tech/LCSHHETE.java
@@ -0,0 +1,16 @@
+package com.afg.tech;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Created by AFlyingGrayson on 8/20/17
+ */
+
+@Mod(modid = LCSHHETE.MODID, name = "LucraftCore: Speedster Heroes: Heroes Expansion: Tech Expansion", version = LCSHHETE.VERSION)
+@Mod.EventBusSubscriber
+public class LCSHHETE
+{
+	public static final String MODID = "g-tech";
+	public static final String VERSION = "1.0";
+
+}
